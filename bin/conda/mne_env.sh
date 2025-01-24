497921672816d68a8f30fc67f5eb95fa1c071dc8
ryoiwata <ryoiwata@github.com>
2025-01-24 08:02:55 -0500

    nb: input metadata

---
+++ bin/conda/mne_env.sh
@@ -0,0 +1,6 @@
+conda create --channel=conda-forge --strict-channel-priority -p ./mne mne-base
+pip install "mne[hdf5]" 
+conda install conda-forge::jupyterlab --yes
+conda install conda-forge::pandas --yes
+conda install conda-forge::openpyxl --yes
+
