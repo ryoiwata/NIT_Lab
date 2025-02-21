afffa8b16e34ff3852980b10d2bb7a3f285ce738
ryoiwata <ryoiwata@github.com>
2025-02-21 12:56:55 -0500

    nb: processing with a DF

--- bin/conda/mne_env.sh
+++ bin/conda/mne_env.sh
@@ -4,4 +4,4 @@ conda install conda-forge::jupyterlab --yes
 conda install conda-forge::pandas --yes
 conda install conda-forge::openpyxl --yes
 conda install conda-forge::scikit-learn --yes
-
+conda install -c edeno spectral_connectivity --yes
